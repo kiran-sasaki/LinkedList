8d19501a1d42e9eda56de6dd2c99729375f83266
kiran-sasaki <kiransekar1111@gmail.com>
2022-08-17 11:02:10 +0530

    day14

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace LinkedListDemoPractice
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+
+            list.Add(56);
+            list.Add(30);
+            list.Add(45);
+            list.Add(67);
+        }
+    }
+}
